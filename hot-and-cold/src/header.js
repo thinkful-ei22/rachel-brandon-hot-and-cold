ab2de08353dac1d03f1d62829f28b27af49320f6
rpinto <rachel.lea.pinto@gmail.com>
2018-07-23 13:41:41 -0700

    heading and nav bar css

--- hot-and-cold/src/header.js
+++ hot-and-cold/src/header.js
@@ -5,12 +5,12 @@ import './header.css';
 export default function Header(props){
 
     return (
-    <header className="App-header">
-    <ul className="navBar">
-    <li className ="NewGame"> <h2>New Game</h2></li>
-
-       <li className ="FAQ"> <h2>Help</h2></li>
+    
+    <nav>
+        <ul>
+            <li>New Game</li>
+            <li>Help</li>
         </ul>
-    </header>       
+    </nav>     
     );
 };
